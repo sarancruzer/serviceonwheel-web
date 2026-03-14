f3c1ef447c0957e7937560e9e12679e436972c8a
Saravanan Nandhan <sarancruzer@gmail.com>
2026-03-14 18:56:24 +0530

    prettier changes

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
+  arrowParens: "always",
+  printWidth: 100,
   semi: true,
   singleQuote: false,
+  tabWidth: 2,
   trailingComma: "all",
 };
 
